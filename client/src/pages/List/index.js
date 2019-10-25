0205f6d91cd18f42dca662f01940563b63513690
Planejamento <planejamento@confetti.com.br>
2019-10-25 17:27:19 -0300

    adding to github

--- client/src/pages/List/index.js
+++ client/src/pages/List/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import api from '../../services/api';
 
 export default function List() {
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    async function loadName() {
+      const response = await api.get();
+      const { data } = response;
+
+      setName(data);
+    }
+
+    loadName();
+  }, []);
+
   return (
     <h1>List</h1>
   );
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(cors());
+app.use(express.json());
 
-app.get('/api', (req, res) => res.send('Hello World!'));
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.get('/api', (req, res) => res.json({ hello : 'dude'}));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
