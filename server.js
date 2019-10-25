ef1702a508a5a12f3fca11ad4107e7e7b1745b60
Planejamento <planejamento@confetti.com.br>
2019-10-25 16:35:29 -0300

    react attempt n4

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "client", "build")))
 
-// app.get('/api', (req, res) => res.send('Hello World!'));
+app.get('/api', (req, res) => res.send('Hello World!'));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
