ba348e8e537e71f7f5658de542641f4b0ec4c55e
Planejamento <planejamento@confetti.com.br>
2019-10-25 15:59:43 -0300

    react and api, first attempt

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/api', (req, res) => res.send('Hello World!'));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+  
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
