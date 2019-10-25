b0b2e0b6dbd046483d2936b28b0ab0e5a152fba1
Planejamento <planejamento@confetti.com.br>
2019-10-25 16:04:21 -0300

    deploy full app

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/api', (req, res) => res.send('Hello World!'));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
   
